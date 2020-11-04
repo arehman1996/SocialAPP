ae02cea24a941e01ab80816b06ea9112e3263592
Ammar <amm4r@live.com>
2020-11-04 23:32:22 +0000

    End of Section &

--- socialapp-spa/src/app/app.module.ts
+++ socialapp-spa/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,6 +12,11 @@ import { NavComponent } from './nav/nav.component';
 import { AuthService} from './_services/auth.service';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { appRoutes } from './routes';
+
 
 
 @NgModule({
@@ -19,14 +25,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       HomeComponent,
       RegisterComponent,
       NavComponent,
-
+      ListsComponent,
+      MessagesComponent,
+      MemberListComponent
    ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AuthService,
